e79281b575d11266c7bd7cd76d54fdee89e0bf47
Nick Quinlan <nicholas.quinlan@sendgrid.com>
2014-01-20 15:26:23 -0800

    removed compass
    right now, compass isn't doing anything, just causing problems, you may
    want to add this back later.

--- basic.rb
+++ basic.rb
@@ -1,5 +1,4 @@
 require 'rubygems'
-require 'compass' #must be loaded before sinatra
 require 'httparty'
 require './static/indexkey.rb'
 require './static/weather.rb'
@@ -10,15 +9,6 @@ require 'sinatra'
 set :views, "views"
 set :public, 'static'
 
-configure do
-  Compass.add_project_configuration(File.join(Sinatra::Application.root, 'config', 'compass.config'))
-end
-
-# at a minimum, the main sass file must reside within the ./views directory. here, we create a ./views/stylesheets directory where all of the sass files can safely reside.
-get '/stylesheets/:name.css' do
-  content_type 'text/css', :charset => 'utf-8'
-  sass(:"stylesheets/#{params[:name]}", Compass.sass_engine_options )
-end
 
 get '/' do
   erb :indexed
